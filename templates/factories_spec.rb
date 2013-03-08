84c3451f05308f3745bd57932b6bc11d828d2128
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-03-08 10:50:22 -0500

    Use more verbose factories spec
    Use code from factory girl wiki testing factories are producting valid
    models. See issue #123.

--- templates/factories_spec.rb
+++ templates/factories_spec.rb
@@ -3,11 +3,10 @@ require 'spec_helper'
 describe 'validate FactoryGirl factories' do
   FactoryGirl.factories.each do |factory|
     context "with factory for :#{factory.name}" do
-      subject { FactoryGirl.build(factory.name) }
+      subject { build(factory.name) }
 
       it 'is valid' do
-        is_valid = subject.valid?
-        expect(is_valid).to be_true, subject.errors.full_messages.join(',')
+        expect(subject).to be_valid, subject.errors.full_messages.join(',')
       end
     end
   end
